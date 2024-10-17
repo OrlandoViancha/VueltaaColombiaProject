fe9172dae1a5ee6b78170e24473b88db97c6265d
orviant20011904 <ortchviant@gmail.com>
2024-10-17 16:34:43 -0500

    Implementacion de Repositorios
    Implementacion de los repositorio para las clases Equipo, Historial Equipo y Patrocinador

---
+++ JavaProject/src/main/java/com/uptc/frw/javaproject/Repositorio/PatrocinadorRepo.java
@@ -0,0 +1,7 @@
+package com.uptc.frw.javaproject.Repositorio;
+
+import com.uptc.frw.javaproject.modelo.Patrocinador;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PatrocinadorRepo extends JpaRepository<Patrocinador, Long> {
+}
