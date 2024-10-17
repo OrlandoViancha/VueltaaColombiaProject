6b61bf8a09a70048b31e25aa28cb05549dd4e7d3
David <dmgorraiz@gmail.com>
2024-10-17 11:58:29 -0500

    CRUD podio

--- JavaProject/src/main/java/com/uptc/frw/javaproject/Controlador/PodioControlador.java
+++ JavaProject/src/main/java/com/uptc/frw/javaproject/Controlador/PodioControlador.java
@@ -1,9 +1,11 @@
 package com.uptc.frw.javaproject.Controlador;
 
 import com.uptc.frw.javaproject.Servicio.PodioServicio;
+import com.uptc.frw.javaproject.modelo.Podio;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("podios")
@@ -11,5 +13,30 @@ public class PodioControlador {
 
     @Autowired
     private PodioServicio podioServicio;
+
+    @GetMapping
+    public List<Podio> listarPodios() {
+        return podioServicio.listar();
+    }
+
+    @GetMapping("{id}")
+    public Podio buscarPodio(@PathVariable long id) {
+        return podioServicio.buscar(id);
+    }
+
+    @PostMapping
+    public Podio crearPodio(@RequestBody Podio podio) {
+        return podioServicio.guardar(podio);
+    }
+
+    @DeleteMapping("{id}")
+    public void eliminarPodio(@PathVariable long id) {
+        podioServicio.borrar(id);
+    }
+
+    @PutMapping
+    public Podio modificarPodio(@RequestParam long id, @RequestBody Podio podio) {
+        return podioServicio.actualizar(id, podio);
+    }
 
 }
